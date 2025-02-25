948f9faa276c75a935c6aad54dfa34afe20a2b85
JSaprah <j.sapraa@gmail.com>
2025-02-25 19:45:49 +0000

    Refer from product page to the detailed screen fix

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 
 def all_products(request):
-    """ Returns all products """
+    """ A view to return a list of all products """
 
     products = Product.objects.all()
 
@@ -14,3 +14,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to return the individual product details based on the product id """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
