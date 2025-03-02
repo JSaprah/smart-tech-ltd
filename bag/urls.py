dc30a3662341db2353c5f6c58b2468d3de2f7dc4
JSaprah <j.sapraa@gmail.com>
2025-03-02 14:40:54 +0000

    Created view for adding products to the basket

--- bag/urls.py
+++ bag/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_product_to_bag, name='add_product_to_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,21 @@ def view_bag(request):
     """ A view to render the bag contents """
 
     return render(request, 'bag/bag.html')
+
+
+def add_product_to_bag(request, product_id):
+    """ A view to add products to the bag with the selected quantity """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] += quantity
+    else:
+        bag[product_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    
+    return redirect(redirect_url)
