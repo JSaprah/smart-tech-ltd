7c05b15312a0e70ba9690abf5541c7306be860fb
JSaprah <j.sapraa@gmail.com>
2025-03-02 16:30:39 +0000

    Updated the context file to add products to the basket#

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,25 +1,41 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
 
-    bag_items = []
-    total = 0
+    bag_products = []
+    bag_total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    for product_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=product_id)
+        bag_total += product.price * quantity
+        product_count += quantity
+        bag_products.append({
+            'product_id': product_id,
+            'quantity': quantity,
+            'product': product,
+
+        })
+
+    if bag_total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = bag_total * Decimal(
+            settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            )
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - bag_total
     else:
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = delivery + total
+    grand_total = delivery + bag_total
 
     context = {
-        'bag_items': bag_items,
-        'total': total,
+        'bag_products': bag_products,
+        'total': bag_total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
--- bag/views.py
+++ bag/views.py
@@ -22,6 +22,5 @@ def add_product_to_bag(request, product_id):
         bag[product_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
-    
+
     return redirect(redirect_url)
