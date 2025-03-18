d0df2f342cd8077e9384250fe0cb7caf9e3cc4dc
JSaprah <j.sapraa@gmail.com>
2025-03-18 22:30:00 +0000

    Completed the checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -9,10 +9,10 @@ def checkout(request):
         messages.error(request, "There are no products in the bag")
         return redirect(reverse('products'))
 
-    form = OrderForm()
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': form,
+        'order_form': order_form,
     }
 
     return render(request, template, context)
