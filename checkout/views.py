a84f7533416342193468fd1105494e1284030615
JSaprah <j.sapraa@gmail.com>
2025-03-19 10:32:17 +0000

    Added Stripe payment method

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QyDxQ2N6Rlkdg5uFstd1SMHJJBgw5bMHgdHlJ5jEIkbgpHaCpHuWVjFqSsabCWs4W0iwKe8OQbtmKotLU1nHd9S00O5JVVZAk',
+        'client_secret': 'sk_test_51QyDxQ2N6Rlkdg5uXDBFWQZSDO07jOc20fWgmVY76BvGZClKukArVIQMZAkClVJ1tPnbgqCNWfwS54YN9F5cbUuI00eLinfh0r',
     }
 
     return render(request, template, context)
