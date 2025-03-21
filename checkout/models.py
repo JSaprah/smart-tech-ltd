42466e153a11685a02f6555b1617e74aaee17649
JSaprah <j.sapraa@gmail.com>
2025-03-21 17:15:38 +0000

    Updated the layout of the allauth templates

--- checkout/models.py
+++ checkout/models.py
@@ -6,12 +6,15 @@ from django.conf import settings
 
 from products.models import Product
 from django_countries.fields import CountryField
+from profiles.models import UserProfile
 
 # Create your models here.
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     first_name = models.CharField(max_length=20, null=False, blank=False)
     last_name = models.CharField(max_length=20, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
