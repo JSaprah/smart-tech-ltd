946fb454499e92ece04fac4eeeb2679086d39a85
JSaprah <j.sapraa@gmail.com>
2025-03-24 20:40:04 +0000

    Added the add and remove functionality for the wishlist

--- wishlist/models.py
+++ wishlist/models.py
@@ -6,6 +6,18 @@ from products.models import Product
 
 
 class Wishlist(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='wishlist_item')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='wishlist_item')
+    added_on = models.DateTimeField(auto_now_add=True)
+
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('wishlist', 'product')
+
+
+
+
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,14 +1,71 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Wishlist
+from .models import Wishlist, WishlistItem
 from products.models import Product
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
 
 @login_required
 def wishlist(request):
+    """ A view to display the wishlist """
 
+    # Fetch the wishlist associated with the logged-in user
+    try:
+        wishlist = Wishlist.objects.get(user=request.user)
+        # Retrieve all the items in the wishlist
+        wishlist_items = WishlistItem.objects.filter(wishlist=wishlist)
+    except Wishlist.DoesNotExist:
+        wishlist_items = []  # If the wishlist doesn't exist, show an empty list
+
+    context = {'wishlist_items': wishlist_items}
     template = 'wishlist/wishlist.html'
 
-    return render(request, template)
+    return render(request, template, context)
+
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """ A view to add products to the wishlist """
+    product = get_object_or_404(Product, id=product_id)
+
+    wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+
+    wishlist_item, item_created = WishlistItem.objects.get_or_create(
+        wishlist=wishlist,
+        product=product
+    )
+
+    if item_created:
+        messages.success(request, "Added to your wishlist.")
+    else:
+        messages.info(request, "This product is already in your wishlist.")
+
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+
+
+@login_required
+def remove_from_wishlist(request, product_id):
+    """ A view to remove products from the wishlist """
+
+    # Get the user's wishlist
+    wishlist = Wishlist.objects.get(user=request.user)
+
+    # Attempt to delete the item
+    deleted_count, _ = WishlistItem.objects.filter(wishlist=wishlist, product_id=product_id).delete()
+    
+    if deleted_count > 0:
+        messages.info(request, "Removed from your wishlist.")
+    else:
+        messages.warning(request, "Item could not be removed.")
+
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+
+
+
+
+
